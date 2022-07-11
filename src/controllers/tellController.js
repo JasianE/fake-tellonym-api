8975f9a2179d6278ba1d5796d1e4f612feb058fb
Jun Li <junchenli423@gmail.com>
2022-07-11 18:18:28 -0400

    ok

--- src/controllers/tellController.js
+++ src/controllers/tellController.js
@@ -4,17 +4,7 @@ exports.add = function(req,res,next){
     console.log(req.body)
     console.log(req)
 
-    const tell = new Tell({
-        message: req.body.message,
-        date: new Date()
-    }).save(err => {
-        if(err){
-            //console.log(err)
-            return next(err)
-        } else {
-            res.send(200)
-        }
-    })
+    res.send(200)
 }
 
 exports.find = function(req,res,next){
